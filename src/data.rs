78a7def210f00d06cec135435896c9a02190a010
arlohb <arlo@urwick.co.uk>
2022-05-15 22:32:02 +0100

    Switched to a tab size of 4

--- src/data.rs
+++ src/data.rs
@@ -2,121 +2,121 @@ use chrono::prelude::*;
 use crate::xml_utils::{get_child, get_child_text};
 
 pub enum DirectionRef {
-  Outbound,
-  Inbound,
+    Outbound,
+    Inbound,
 }
 
 pub struct FramedVehicleJourneyRef {
-  pub data_frame_ref: String,
-  pub dated_vehicle_journey_ref: String,
+    pub data_frame_ref: String,
+    pub dated_vehicle_journey_ref: String,
 }
 
 pub struct Location {
-  pub longitude: f64,
-  pub latitude: f64,
+    pub longitude: f64,
+    pub latitude: f64,
 }
 
 pub struct MonitoredVehicleJourney {
-  pub line_ref: String,
-  pub direction_ref: DirectionRef,
-  pub framed_vehicle_journey_ref: FramedVehicleJourneyRef,
-  pub published_line_name: String,
-  pub operator_ref: String,
-  pub origin_ref: String,
-  pub origin_name: String,
-  pub destination_ref: String,
-  pub destination_name: String,
-  pub origin_aimed_departure_time: DateTime<Utc>,
-  pub vehicle_location: Location,
-  pub bearing: Option<f64>,
-  pub vehicle_ref: String,
+    pub line_ref: String,
+    pub direction_ref: DirectionRef,
+    pub framed_vehicle_journey_ref: FramedVehicleJourneyRef,
+    pub published_line_name: String,
+    pub operator_ref: String,
+    pub origin_ref: String,
+    pub origin_name: String,
+    pub destination_ref: String,
+    pub destination_name: String,
+    pub origin_aimed_departure_time: DateTime<Utc>,
+    pub vehicle_location: Location,
+    pub bearing: Option<f64>,
+    pub vehicle_ref: String,
 }
 
 pub struct VehicleJourney {
-  pub driver_ref: u32,
+    pub driver_ref: u32,
 }
 
 pub struct Extensions {
-  pub vehicle_journey: VehicleJourney,
+    pub vehicle_journey: VehicleJourney,
 }
 
 pub struct VehicleActivity {
-  pub recorded_at_time: DateTime<Utc>,
-  pub item_identifier: String,
-  pub valid_until_time: DateTime<Utc>,
-  pub monitored_vehicle_journey: MonitoredVehicleJourney,
-  pub extensions: Extensions,
+    pub recorded_at_time: DateTime<Utc>,
+    pub item_identifier: String,
+    pub valid_until_time: DateTime<Utc>,
+    pub monitored_vehicle_journey: MonitoredVehicleJourney,
+    pub extensions: Extensions,
 }
 
 impl VehicleActivity {
-  pub fn from_node(node: &roxmltree::Node) -> Option<VehicleActivity> {
-    Some(VehicleActivity {
-      recorded_at_time: get_child_text(node, "RecordedAtTime")?
-        .parse::<DateTime<Utc>>().ok()?,
-      item_identifier: get_child_text(node, "ItemIdentifier")?,
-      valid_until_time: (get_child_text(node, "ValidUntilTime")?
-        .split(".").nth(0)?
-        .to_string()
-        + "Z")
-        .parse::<DateTime<Utc>>().ok()?,
-      monitored_vehicle_journey: {
-        let node = &get_child(node, "MonitoredVehicleJourney")?;
+    pub fn from_node(node: &roxmltree::Node) -> Option<VehicleActivity> {
+        Some(VehicleActivity {
+            recorded_at_time: get_child_text(node, "RecordedAtTime")?
+                .parse::<DateTime<Utc>>().ok()?,
+            item_identifier: get_child_text(node, "ItemIdentifier")?,
+            valid_until_time: (get_child_text(node, "ValidUntilTime")?
+                .split(".").nth(0)?
+                .to_string()
+                + "Z")
+                .parse::<DateTime<Utc>>().ok()?,
+            monitored_vehicle_journey: {
+                let node = &get_child(node, "MonitoredVehicleJourney")?;
 
-        MonitoredVehicleJourney {
-          line_ref: get_child_text(node, "LineRef")?
-            .to_string(),
-          direction_ref: match get_child_text(node, "DirectionRef")?.as_str() {
-              "OUTBOUND" => DirectionRef::Outbound,
-              "INBOUND" => DirectionRef::Inbound,
-              _ => return None,
+                MonitoredVehicleJourney {
+                    line_ref: get_child_text(node, "LineRef")?
+                        .to_string(),
+                    direction_ref: match get_child_text(node, "DirectionRef")?.as_str() {
+                        "OUTBOUND" => DirectionRef::Outbound,
+                        "INBOUND" => DirectionRef::Inbound,
+                        _ => return None,
+                        },
+                    framed_vehicle_journey_ref: {
+                        let node = &get_child(node, "FramedVehicleJourneyRef")?;
+                        
+                        FramedVehicleJourneyRef {
+                            data_frame_ref: get_child_text(node, "DataFrameRef")?,
+                            dated_vehicle_journey_ref: get_child_text(node, "DatedVehicleJourneyRef")?,
+                        }
+                    },
+                    published_line_name: get_child_text(node, "PublishedLineName")?,
+                    operator_ref: get_child_text(node, "OperatorRef")?,
+                    origin_ref: get_child_text(node, "OriginRef")?,
+                    origin_name: get_child_text(node, "OriginName")?,
+                    destination_ref: get_child_text(node, "DestinationRef")?,
+                    destination_name: get_child_text(node, "DestinationName")?,
+                    origin_aimed_departure_time: get_child_text(node, "OriginAimedDepartureTime")?
+                        .parse::<DateTime<Utc>>().ok()?,
+                    vehicle_location: {
+                        let node = &get_child(node, "VehicleLocation")?;
+
+                        Location {
+                            longitude: get_child_text(node, "Longitude")?
+                                .parse::<f64>().ok()?,
+                            latitude: get_child_text(node, "Latitude")?
+                                .parse::<f64>().ok()?,
+                        }
+                    },
+                    bearing: match get_child_text(node, "Bearing") {
+                        Some(node) => Some(node
+                        .parse::<f64>().ok()?),
+                        None => None,
+                    },
+                    vehicle_ref: get_child_text(node, "VehicleRef")?,
+                }
             },
-          framed_vehicle_journey_ref: {
-            let node = &get_child(node, "FramedVehicleJourneyRef")?;
-            
-            FramedVehicleJourneyRef {
-              data_frame_ref: get_child_text(node, "DataFrameRef")?,
-              dated_vehicle_journey_ref: get_child_text(node, "DatedVehicleJourneyRef")?,
-            }
-          },
-          published_line_name: get_child_text(node, "PublishedLineName")?,
-          operator_ref: get_child_text(node, "OperatorRef")?,
-          origin_ref: get_child_text(node, "OriginRef")?,
-          origin_name: get_child_text(node, "OriginName")?,
-          destination_ref: get_child_text(node, "DestinationRef")?,
-          destination_name: get_child_text(node, "DestinationName")?,
-          origin_aimed_departure_time: get_child_text(node, "OriginAimedDepartureTime")?
-            .parse::<DateTime<Utc>>().ok()?,
-          vehicle_location: {
-            let node = &get_child(node, "VehicleLocation")?;
+            extensions: {
+                let node = &get_child(node, "Extensions")?;
+                let node = &get_child(node, "VehicleJourney")?;
 
-            Location {
-              longitude: get_child_text(node, "Longitude")?
-                .parse::<f64>().ok()?,
-              latitude: get_child_text(node, "Latitude")?
-                .parse::<f64>().ok()?,
-            }
-          },
-          bearing: match get_child_text(node, "Bearing") {
-            Some(node) => Some(node
-              .parse::<f64>().ok()?),
-            None => None,
-          },
-          vehicle_ref: get_child_text(node, "VehicleRef")?,
-        }
-      },
-      extensions: {
-        let node = &get_child(node, "Extensions")?;
-        let node = &get_child(node, "VehicleJourney")?;
-
-        Extensions {
-          vehicle_journey: VehicleJourney {
-            driver_ref: get_child_text(node, "DriverRef")?
-              .parse::<u32>()
-              .or::<Result<u32, std::num::ParseIntError>>(Ok(0))
-              .ok()?,
-          },
-        }
-      },
-    })
-  }
+                Extensions {
+                    vehicle_journey: VehicleJourney {
+                        driver_ref: get_child_text(node, "DriverRef")?
+                            .parse::<u32>()
+                            .or::<Result<u32, std::num::ParseIntError>>(Ok(0))
+                            .ok()?,
+                    },
+                }
+            },
+        })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,81 +3,81 @@ mod xml_utils;
 
 #[derive(Debug)]
 pub enum Err {
-  Reqwest,
-  Xml,
+    Reqwest,
+    Xml,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Err> {
-  let client = reqwest::Client::new();
-  let res = client
-    .get(format!(
-      "https://data.bus-data.dft.gov.uk/api/v1/datafeed/1695/?api_key={}",
-      include_str!("../api_key.txt")
-    ))
-    .send()
-    .await
-    .unwrap();
+    let client = reqwest::Client::new();
+    let res = client
+        .get(format!(
+            "https://data.bus-data.dft.gov.uk/api/v1/datafeed/1695/?api_key={}",
+            include_str!("../api_key.txt")
+        ))
+        .send()
+        .await
+        .unwrap();
 
-  let text = res.text().await.map_err(|_| Err::Reqwest)?;
+    let text = res.text().await.map_err(|_| Err::Reqwest)?;
 
-  let doc = roxmltree::Document::parse(&text).unwrap();
+    let doc = roxmltree::Document::parse(&text).unwrap();
 
-  let elements = doc.root()
-    .first_child().ok_or(Err::Xml)?      // Siri
-    .first_child().ok_or(Err::Xml)?      // ServiceDelivery
-    .children().nth(2).ok_or(Err::Xml)?  // VehicleMonitoringDelivery
-    .children().filter(|node|
-      node.tag_name().name() == "VehicleActivity"
-    )                                    // VehicleActivity
-    .collect::<Vec<_>>();
+    let elements = doc.root()
+        .first_child().ok_or(Err::Xml)?     // Siri
+        .first_child().ok_or(Err::Xml)?     // ServiceDelivery
+        .children().nth(2).ok_or(Err::Xml)? // VehicleMonitoringDelivery
+        .children().filter(|node|
+            node.tag_name().name() == "VehicleActivity"
+        )                                   // VehicleActivity
+        .collect::<Vec<_>>();
 
-  let mut activities = elements
-    .iter()
-    .map(|node| data::VehicleActivity::from_node(&node))
-    .filter(|option| option.is_some())
-    .map(|option| option.unwrap())
-    .collect::<Vec<_>>();
-  
-  println!("xml: {}, parsed: {}", elements.len(), activities.len());
+    let mut activities = elements
+        .iter()
+        .map(|node| data::VehicleActivity::from_node(&node))
+        .filter(|option| option.is_some())
+        .map(|option| option.unwrap())
+        .collect::<Vec<_>>();
+    
+    println!("xml: {}, parsed: {}", elements.len(), activities.len());
 
-  activities.sort_by(|a, b|
-    a.monitored_vehicle_journey.line_ref.cmp(&b.monitored_vehicle_journey.line_ref)
-  );
-  
-  let mut map = staticmap::StaticMapBuilder::new()
-    .width(6000)
-    .height(9000)
-    .build()
-    .unwrap();
-  
-  activities
-    .iter()
-    .for_each(|activity| {
-      let lat = activity.monitored_vehicle_journey.vehicle_location.latitude;
-      let long = activity.monitored_vehicle_journey.vehicle_location.longitude;
-      println!("{:4} {:<10} {:<10}",
-        activity.monitored_vehicle_journey.line_ref.as_str(),
-        lat,
-        long,
-      );
+    activities.sort_by(|a, b|
+        a.monitored_vehicle_journey.line_ref.cmp(&b.monitored_vehicle_journey.line_ref)
+    );
+    
+    let mut map = staticmap::StaticMapBuilder::new()
+        .width(6000)
+        .height(9000)
+        .build()
+        .unwrap();
+    
+    activities
+        .iter()
+        .for_each(|activity| {
+            let lat = activity.monitored_vehicle_journey.vehicle_location.latitude;
+            let long = activity.monitored_vehicle_journey.vehicle_location.longitude;
+            println!("{:4} {:<10} {:<10}",
+                activity.monitored_vehicle_journey.line_ref.as_str(),
+                lat,
+                long,
+            );
 
-      if lat == 0. || long == 0. {
-        return;
-      }
+            if lat == 0. || long == 0. {
+                return;
+            }
 
-      map.add_tool(
-        staticmap::tools::CircleBuilder::new()
-          .lat_coordinate(lat)
-          .lon_coordinate(long)
-          .color(staticmap::tools::Color::new(false, 0, 0, 255, 255))
-          .radius(2.)
-          .build()
-          .unwrap()
-      )
-    });
-  
-  map.save_png("staticmap.png").unwrap();
+            map.add_tool(
+                staticmap::tools::CircleBuilder::new()
+                .lat_coordinate(lat)
+                .lon_coordinate(long)
+                .color(staticmap::tools::Color::new(false, 0, 0, 255, 255))
+                .radius(2.)
+                .build()
+                .unwrap()
+            )
+        });
+    
+    map.save_png("staticmap.png").unwrap();
 
-  Ok(())
+    Ok(())
 }
